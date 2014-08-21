d741c82afedd0c0431d76bbe610c2f2efb4a9355
Markus Graube <markus.graube@tu-dresden.de>
2014-08-21 13:19:32 +0200

    fix coverity issues

--- test/src/de/tud/plt/r43ples/test/TestR43ples.java
+++ test/src/de/tud/plt/r43ples/test/TestR43ples.java
@@ -11,6 +11,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpException;
 import org.apache.log4j.Logger;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -68,110 +69,113 @@ public class TestR43ples {
 		// Purge silent example graph
 		logger.info("Purge silent example graph");
 		String query = String.format("DROP SILENT GRAPH <%s>", graphName);
-		logger.info("Execute query: \n" + query);
-		logger.info("Response: \n" + executeR43plesQuery(query));
+		logger.debug("Execute query: \n" + query);
+		logger.debug("Response: \n" + executeR43plesQuery(query));
 		
 		// Create new example graph
 		logger.info("Create new example graph");
 		query = String.format("CREATE SILENT GRAPH <%s>", graphName);
-		logger.info("Execute query: \n" + query);
-		logger.info("Response: \n" + executeR43plesQuery(query));
+		logger.debug("Execute query: \n" + query);
+		logger.debug("Response: \n" + executeR43plesQuery(query));
 		
 		// Initial commit
 		logger.info("Initial commit");
 		query = String.format(""
-				+ "USER \"shensel\" \n"
-				+ "MESSAGE \"Initial commit.\" \n"
-				+ "INSERT DATA INTO <%s> REVISION \"0\" \n"
-				+ "{ \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"A\". \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"B\". \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"C\". \n"
+				+ "USER \"shensel\" %n"
+				+ "MESSAGE \"Initial commit.\" %n"
+				+ "INSERT DATA INTO <%s> REVISION \"0\" %n"
+				+ "{"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"A\". %n"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"B\". %n"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"C\". %n"
 				+ "}", graphName);
-		logger.info("Execute query: \n" + query);
-		logger.info("Response: \n" + executeR43plesQuery(query));
+		logger.debug("Execute query: \n" + query);
+		logger.debug("Response: \n" + executeR43plesQuery(query));
 		
 		// Create a new branch B1
 		logger.info("Create a new branch B1");
 		query = String.format(""
-				+ "USER \"shensel\" \n"
-				+ "MESSAGE \"Branch B1.\" \n"
-				+ "BRANCH GRAPH <%s> REVISION \"1\" TO \"B1\" \n", graphName);
-		logger.info("Execute query: \n" + query);
-		logger.info("Response: \n" + executeR43plesQuery(query));
+				+ "USER \"shensel\" %n"
+				+ "MESSAGE \"Branch B1.\" %n"
+				+ "BRANCH GRAPH <%s> REVISION \"1\" TO \"B1\"", graphName);
+		logger.debug("Execute query: \n" + query);
+		logger.debug("Response: \n" + executeR43plesQuery(query));
 		
 		// Create a new branch B1
 		logger.info("Create a new branch B2");
 		query = String.format(""
-				+ "USER \"shensel\" \n"
-				+ "MESSAGE \"Branch B2.\" \n"
-				+ "BRANCH GRAPH <%s> REVISION \"1\" TO \"B2\" \n", graphName);
-		logger.info("Execute query: \n" + query);
-		logger.info("Response: \n" + executeR43plesQuery(query));
+				+ "USER \"shensel\" %n"
+				+ "MESSAGE \"Branch B2.\" %n"
+				+ "BRANCH GRAPH <%s> REVISION \"1\" TO \"B2\"", graphName);
+		logger.debug("Execute query: \n" + query);
+		logger.debug("Response: \n" + executeR43plesQuery(query));
 		
 		// First commit to B1
 		logger.info("First commit to B1");
 		query = String.format(""
-				+ "USER \"shensel\" \n"
-				+ "MESSAGE \"First commit to B1.\" \n"
-				+ "INSERT DATA INTO <%s> REVISION \"B1\" \n"
-				+ "{ \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"D\". \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"E\". \n"
+				+ "USER \"shensel\" %n"
+				+ "MESSAGE \"First commit to B1.\" %n"
+				+ "INSERT DATA INTO <%s> REVISION \"B1\" %n"
+				+ "{ %n"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"D\". %n"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"E\". %n"
 				+ "}"
-				+ "DELETE DATA FROM <%s> REVISION \"B1\" \n"
-				+ "{ \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"A\". \n"
+				+ "DELETE DATA FROM <%s> REVISION \"B1\" %n"
+				+ "{ %n"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"A\". %n"
 				+ "}", graphName, graphName);
-		logger.info("Execute query: \n" + query);
-		logger.info("Response: \n" + executeR43plesQuery(query));
+		logger.debug("Execute query: \n" + query);
+		logger.debug("Response: \n" + executeR43plesQuery(query));
 		
 		// First commit to B2
 		logger.info("First commit to B2");
 		query = String.format(""
-				+ "USER \"shensel\" \n"
-				+ "MESSAGE \"First commit to B2.\" \n"
-				+ "INSERT DATA INTO <%s> REVISION \"B2\" \n"
-				+ "{ \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"D\". \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"H\". \n"
+				+ "USER \"shensel\" %n"
+				+ "MESSAGE \"First commit to B2.\" %n"
+				+ "INSERT DATA INTO <%s> REVISION \"B2\" %n"
+				+ "{ %n"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"D\". %n"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"H\". %n"
 				+ "}"
-				+ "DELETE DATA FROM <%s> REVISION \"B2\" \n"
-				+ "{ \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"C\". \n"
+				+ "DELETE DATA FROM <%s> REVISION \"B2\" %n"
+				+ "{ %n"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"C\". %n"
 				+ "}", graphName, graphName);
-		logger.info("Execute query: \n" + query);
-		logger.info("Response: \n" + executeR43plesQuery(query));
+		logger.debug("Execute query: \n" + query);
+		logger.debug("Response: \n" + executeR43plesQuery(query));
 		
 		// Second commit to B1
 		logger.info("Second commit to B1");
 		query = String.format(""
-				+ "USER \"shensel\" \n"
-				+ "MESSAGE \"Second commit to B1.\" \n"
-				+ "INSERT DATA INTO <%s> REVISION \"B1\" \n"
-				+ "{ \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"G\". \n"
+				+ "USER \"shensel\" %n"
+				+ "MESSAGE \"Second commit to B1.\" %n"
+				+ "INSERT DATA INTO <%s> REVISION \"B1\" %n"
+				+ "{ %n"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"G\". %n"
 				+ "}"
-				+ "DELETE DATA FROM <%s> REVISION \"B1\" \n"
-				+ "{ \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"D\". \n"
+				+ "DELETE DATA FROM <%s> REVISION \"B1\" %n"
+				+ "{ %n"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"D\". %n"
 				+ "}", graphName, graphName);
-		logger.info("Execute query: \n" + query);
-		logger.info("Response: \n" + executeR43plesQuery(query));
+		logger.debug("Execute query: \n" + query);
+		logger.debug("Response: \n" + executeR43plesQuery(query));
 		
 		// Second commit to B2
 		logger.info("Second commit to B2");
 		query = String.format(""
-				+ "USER \"shensel\" \n"
-				+ "MESSAGE \"Second commit to B2.\" \n"
-				+ "INSERT DATA INTO <%s> REVISION \"B2\" \n"
-				+ "{ \n"
-				+ "  <http://example.com/testS> <http://example.com/testP> \"I\". \n"
+				+ "USER \"shensel\" %n"
+				+ "MESSAGE \"Second commit to B2.\" %n"
+				+ "INSERT DATA INTO <%s> REVISION \"B2\" %n"
+				+ "{ %n"
+				+ "  <http://example.com/testS> <http://example.com/testP> \"I\". %n"
 				+ "}", graphName);
-		logger.info("Execute query: \n" + query);
-		logger.info("Response: \n" + executeR43plesQuery(query));
-		
-		logger.info("Example graph created.");
+		logger.debug("Execute query: \n" + query);
+		logger.debug("Response: \n" + executeR43plesQuery(query));
+	}
+	
+	@Test public void testServiceDescription() throws IOException{
+		String result = executeR43plesQueryWithFormat("", "text/turtle");
+		Assert.assertNotNull(result);
 	}
 	
 	
@@ -183,9 +187,20 @@ public class TestR43ples {
 	 * @throws IOException 
 	 */
 	public static String executeR43plesQuery(String query) throws IOException {
+		return executeR43plesQueryWithFormat(query, "");
+	}
+	
+	/**
+	 * Executes a SPARQL-query against the R43ples endpoint
+	 * 
+	 * @param query the SPARQL query
+	 * @return the result of the query
+	 * @throws IOException 
+	 */
+	public static String executeR43plesQueryWithFormat(String query, String format) throws IOException {
 		URL url = null;
 		
-		url = new URL(endpoint+ "?query=" + URLEncoder.encode(query, "UTF-8"));
+		url = new URL(endpoint+ "?query=" + URLEncoder.encode(query, "UTF-8")+ "&format=" + URLEncoder.encode(format, "UTF-8") );
 		logger.debug(url.toString());
 
 		URLConnection con = null;
