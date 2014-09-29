458c9b17c8e04bf2fb431c2e5026a47b5c317c41
Markus Graube <markus.graube@tu-dresden.de>
2014-09-29 09:02:24 +0200

    generate real dist confiog file

--- src/de/tud/plt/r43ples/management/Config.java
+++ src/de/tud/plt/r43ples/management/Config.java
@@ -2,6 +2,7 @@ package de.tud.plt.r43ples.management;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.log4j.Logger;
 
 public class Config {
 	
@@ -13,7 +14,13 @@ public class Config {
 	public static String yed_filepath;
 	public static String visualisation_path;
 	public static String revision_graph;
+	
+	private static Logger logger = Logger.getLogger(Config.class);
 
+	
+	public static void readDefaultConfig() throws ConfigurationException {
+		readConfig("r43ples.conf");
+	}
 	
 	/**
 	 * read the configuration information from local file
@@ -22,18 +29,25 @@ public class Config {
 	 */
 	public static void readConfig(final String configFilePath) throws ConfigurationException{
 		
-		PropertiesConfiguration config = new PropertiesConfiguration(configFilePath);
-		service_uri = config.getString("service.uri");
-		service_port = config.getInt("service.port");
+		PropertiesConfiguration config;
+		try {
+			config = new PropertiesConfiguration(configFilePath);
+			service_uri = config.getString("service.uri");
+			service_port = config.getInt("service.port");
+			
+			sparql_endpoint = config.getString("sparql.endpoint");
+			sparql_user = config.getString("sparql.username");
+			sparql_password = config.getString("sparql.password");
+			
+			revision_graph = config.getString("revision.graph");
+			
+			yed_filepath = config.getString("yEd.filePath");
+			visualisation_path = config.getString("visualisation.path");
+		} catch (ConfigurationException e) {
+			logger.warn("Could not read configuration file '" + configFilePath + "'. Switch to 'r43ples.dist.conf'.");
+			readConfig("r43ples.dist.conf");
+		}
 		
-		sparql_endpoint = config.getString("sparql.endpoint");
-		sparql_user = config.getString("sparql.username");
-		sparql_password = config.getString("sparql.password");
-		
-		revision_graph = config.getString("revision.graph");
-		
-		yed_filepath = config.getString("yEd.filePath");
-		visualisation_path = config.getString("visualisation.path");
 	}
 
 }
