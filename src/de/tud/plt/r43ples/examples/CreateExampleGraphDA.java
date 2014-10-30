829056394c3b8a87d6a541f4da402e9dd96f6e89
stephanhensel <stephhens@gmx.de>
2014-10-30 14:00:35 +0100

    Sample dataset of diploma thesis added

---
+++ src/de/tud/plt/r43ples/examples/CreateExampleGraphDA.java
@@ -0,0 +1,41 @@
+package de.tud.plt.r43ples.examples;
+
+import java.io.IOException;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.http.HttpException;
+
+import de.tud.plt.r43ples.management.Config;
+import de.tud.plt.r43ples.management.SampleDataSet;
+import de.tud.plt.r43ples.management.TripleStoreInterface;
+
+
+/**
+ * Create an example graph.
+ * 
+ * @author Stephan Hensel
+ *
+ */
+public class CreateExampleGraphDA {
+
+	/** The graph name. **/
+	private static String graphName = "http://exampleGraph";
+	
+	
+	/**
+	 * Main entry point. Create the example graph.
+	 * Used in diploma thesis of Stephan Hensel.
+	 * 
+	 * @param args
+	 * @throws IOException 
+	 * @throws ConfigurationException 
+	 * @throws HttpException 
+	 */
+	public static void main(String[] args) throws IOException, ConfigurationException, HttpException {
+		
+		Config.readConfig("r43ples.conf");
+		TripleStoreInterface.init(Config.sparql_endpoint, Config.sparql_user, Config.sparql_password);
+		
+		SampleDataSet.createSampleDataSetDA(graphName);
+	}
+}
--- src/de/tud/plt/r43ples/management/SampleDataSet.java
+++ src/de/tud/plt/r43ples/management/SampleDataSet.java
@@ -140,6 +140,81 @@ public class SampleDataSet {
 
 		logger.info("Example graph created.");
 	}
+	
+	
+	/**
+	 * Create an example graph of the following structure:
+	 * 
+	 *                  ADD: D,E              ADD: G
+	 *               +-----X---------------------X--------- (Branch B1)
+	 *               |  DEL: A                DEL: D
+	 * ADD: A,B,C    |
+	 * ---X----------+ (Master)
+	 * DEL: -        |
+	 *               |  ADD: D,H              ADD: I
+	 *               +-----X---------------------X--------- (Branch B2)
+	 *                  DEL: C                DEL: -
+	 * 
+	 * Used in diploma thesis of Stephan Hensel.
+	 * 
+	 * @author Stephan Hensel
+	 * @throws IOException
+	 * @throws HttpException
+	 *
+	 */
+	public static void createSampleDataSetDA(String graphName) throws IOException, HttpException {
+		/** The user. **/
+		String user = "shensel";
+
+		// Create new example graph
+		DatasetGenerationManagement.createNewGraph(graphName);
+
+		// Initial commit
+		String triples = "<http://example.com/testS> <http://example.com/testP> \"A\". \n"
+				+ "<http://example.com/testS> <http://example.com/testP> \"B\". \n"
+				+ "<http://example.com/testS> <http://example.com/testP> \"C\". \n";
+
+		DatasetGenerationManagement.executeInsertQuery(user, "Initial commit", graphName, "0", triples);
+
+		// Create a new branch B1
+		DatasetGenerationManagement.createNewBranch(user, "Create a new branch B1", graphName, "1", "B1");
+
+		// Create a new branch B2
+		DatasetGenerationManagement.createNewBranch(user, "Create a new branch B2", graphName, "1", "B2");
+
+		// First commit to B1
+		String triplesInsert = "<http://example.com/testS> <http://example.com/testP> \"D\". \n"
+				+ "<http://example.com/testS> <http://example.com/testP> \"E\". \n";
+
+		String triplesDelete = "<http://example.com/testS> <http://example.com/testP> \"A\". \n";
+
+		DatasetGenerationManagement.executeInsertDeleteQuery(user, "First commit to B1", graphName, "B1",
+				triplesInsert, triplesDelete);
+
+		// First commit to B2
+		triplesInsert = "<http://example.com/testS> <http://example.com/testP> \"D\". \n"
+				+ "<http://example.com/testS> <http://example.com/testP> \"H\". \n";
+
+		triplesDelete = "<http://example.com/testS> <http://example.com/testP> \"C\". \n";
+
+		DatasetGenerationManagement.executeInsertDeleteQuery(user, "First commit to B2", graphName, "B2",
+				triplesInsert, triplesDelete);
+
+		// Second commit to B1
+		triplesInsert = "<http://example.com/testS> <http://example.com/testP> \"G\". \n";
+
+		triplesDelete = "<http://example.com/testS> <http://example.com/testP> \"D\". \n";
+
+		DatasetGenerationManagement.executeInsertDeleteQuery(user, "Second commit to B1", graphName, "B1",
+				triplesInsert, triplesDelete);
+
+		// Second commit to B2
+		triplesInsert = "<http://example.com/testS> <http://example.com/testP> \"I\". \n";
+		DatasetGenerationManagement.executeInsertQuery(user, "Second commit to B2", graphName, "B2",
+				triplesInsert);
+
+		logger.info("Example graph created.");
+	}
 
 	
 	/**
