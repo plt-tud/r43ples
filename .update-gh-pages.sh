be8d80c7f2976e1c0a692e3030626ee46c90bbc3
Markus Graube <markus.graube@tu-dresden.de>
2014-12-05 15:27:42 +0100

    Update gh-page on every commit

--- .update-gh-pages.sh
+++ .update-gh-pages.sh
@@ -3,22 +3,16 @@
 # This scripts updates the javadoc and pushes all changes together 
 # with README.md to gh-pages in order to update th website
 
-COMMITS=`git log --since=today.midnight --oneline | wc -l`
-
-if [[ "$COMMITS" -le "1" ]]; then
-    sudo apt-get install -qq graphviz
-    ant javadoc
-    git clone -b gh-pages https://$GITAUTH@github.com/plt-tud/r43ples
-    rm -rf r43ples/javadoc
-    cp -r javadoc r43ples/javadoc
-    echo -e '---\nlayout: index\n---\n' > r43ples/index.md
-    cat README.md >> r43ples/index.md
-    cd r43ples/javadoc
-    git add *
-    git commit -am "javadoc updated by travis-ci"
-    git push https://$GITAUTH@github.com/plt-tud/r43ples
-    cd ..
-    rm -rf r43ples
-else
-    echo "Not the first commit of the day - no push to gh-pages required"
-fi 
+sudo apt-get install -qq graphviz
+ant javadoc
+git clone -b gh-pages https://$GITAUTH@github.com/plt-tud/r43ples
+rm -rf r43ples/javadoc
+cp -r javadoc r43ples/javadoc
+echo -e '---\nlayout: index\n---\n' > r43ples/index.md
+cat README.md >> r43ples/index.md
+cd r43ples/javadoc
+git add *
+git commit -am "javadoc updated by travis-ci"
+git push https://$GITAUTH@github.com/plt-tud/r43ples
+cd ..
+rm -rf r43ples
