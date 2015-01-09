fabc0948cda835f7f48b9cf8ded927606ee7f2db
stephanhensel <stephhens@gmx.de>
2015-01-09 16:08:54 +0100

    SSL code added (currently not working)

--- src/main/java/de/tud/plt/r43ples/webservice/Service.java
+++ src/main/java/de/tud/plt/r43ples/webservice/Service.java
@@ -11,6 +11,8 @@ import org.apache.http.HttpException;
 import org.apache.log4j.Logger;
 import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.ssl.SSLContextConfigurator;
+import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.mvc.mustache.MustacheMvcFeature;
@@ -65,22 +67,54 @@ public class Service {
 	public static void start() throws ConfigurationException, IOException, HttpException, URISyntaxException {
 		logger.info("Starting R43ples on grizzly...");
 		Config.readConfig("r43ples.conf");
-		URI BASE_URI = UriBuilder.fromUri(Config.service_uri).port(Config.service_port).path("r43ples").build();
+		URI BASE_URI = null;
 	
-		ResourceConfig rc = new ResourceConfig()
+		// Choose if the endpoint should be SSL secured
+		if (Config.service_secure) {
+			BASE_URI = UriBuilder.fromUri(Config.service_uri.replaceFirst("http://", "https://")).port(Config.service_port).path("r43ples").build();
+		
+			ResourceConfig rc = new ResourceConfig()
 				.registerClasses(Endpoint.class)
 				.property(MustacheMvcFeature.TEMPLATE_BASE_PATH, "templates")
 				.register(MustacheMvcFeature.class)
 				.register(ExceptionMapper.class);
-		server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
-		server.getServerConfiguration().addHttpHandler(
-		        new CLStaticHttpHandler(Service.class.getClassLoader(),"webapp/"), "/static/");
-		server.start();
+
+			SSLContextConfigurator sslCon = new SSLContextConfigurator();
+			sslCon.setKeyStoreFile(Config.ssl_keystore);
+			sslCon.setKeyStorePass(Config.ssl_password);
+	
+			server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc, true, new SSLEngineConfigurator(sslCon, false, false, false));
+
+			server.getServerConfiguration().addHttpHandler(
+			        new CLStaticHttpHandler(Service.class.getClassLoader(),"webapp/"), "/static/");
+
+			server.start();
+		
+			logger.info("Connection is secure.");
+		} else {
+			BASE_URI = UriBuilder.fromUri(Config.service_uri).port(Config.service_port).path("r43ples").build();
+			
+			ResourceConfig rc = new ResourceConfig()
+				.registerClasses(Endpoint.class)
+				.property(MustacheMvcFeature.TEMPLATE_BASE_PATH, "templates")
+				.register(MustacheMvcFeature.class)
+				.register(ExceptionMapper.class);
+			
+			server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+			
+			server.getServerConfiguration().addHttpHandler(
+			        new CLStaticHttpHandler(Service.class.getClassLoader(),"webapp/"), "/static/");
+			
+			server.start();
+			
+			logger.info("Connection is not secure.");
+		}
+		
 		logger.info(String.format("Server started - R43ples endpoint available under: %s/sparql", BASE_URI));
 		
 		logger.info("Version: "+ Service.class.getPackage().getImplementationVersion());
 		
-		TripleStoreInterface.init(Config.database_directory);//Config.sparql_endpoint, Config.sparql_user, Config.sparql_password);
+		TripleStoreInterface.init(Config.database_directory);
 	}
 	
 	
---
+++ src/main/resources/ssl/generation.bat
@@ -0,0 +1,7 @@
+:: run cmd as admin
+:: go to your java installation directory 
+
+cd C:\Program Files\Java\jre7\bin
+keytool -genkey -alias r43ples -keypass r43ples -keystore C:\keystoreR43ples
+
+:: enter identification: r43ples - PLT - TUD - DD - SN - DE
