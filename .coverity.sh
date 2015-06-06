8443ff1de6cf37a6c33d4e15ff87e5cb9b1a0726
Markus Graube <markus.graube@tu-dresden.de>
2015-06-06 23:29:20 +0200

    Fix coverity script for travis

--- .coverity.sh
+++ .coverity.sh
@@ -5,18 +5,20 @@
 # of the day to the coverity scan service
 #
 
+git fetch origin coverity_scan
 COMMITS=`git log --since=today.midnight --oneline coverity_scan | wc -l`
 
 if [[ "$COMMITS" -le "1" ]]; then
     #first commit a day - push changes to branch coverity_scan
-    
-    git config --global user.email "r43ples-travis-ci@users.noreply.github.com"
-    git config --global user.name "r43ples travis-ci"
-    git config --global push.default simple 
 
     git checkout coverity_scan
     git pull
     git merge --ff --log -m "merge from master to coverity_scan" origin/master
+    
+    git config --global user.email "r43ples-travis-ci@users.noreply.github.com"
+    git config --global user.name "r43ples travis-ci"
+    git config --global push.default simple 
+    
     git push https://$GITAUTH@github.com/plt-tud/r43ples
     git checkout master
 else
