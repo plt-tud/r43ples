f05b6e2a3877bfd697d6b580461ce1dcf0cb48b4
Markus Graube <markus.graube@tu-dresden.de>
2015-06-14 14:13:10 +0200

    prepare version 0.8.7

--- .coverity.sh
+++ .coverity.sh
@@ -9,15 +9,14 @@ COMMITS=`git log --since=today.midnight --oneline | wc -l`
 
 if [[ "$COMMITS" -le "1" ]]; then
     echo "first commit a day - push changes to branch coverity_scan"
-
-    git fetch origin +coverity_scan:coverity_scan
-    git checkout coverity_scan
-    git merge --ff --log -m "merge from master to coverity_scan" origin/master
     
     git config --global user.email "r43ples-travis-ci@users.noreply.github.com"
     git config --global user.name "r43ples travis-ci"
     git config --global push.default simple 
-    
+
+    git fetch origin +coverity_scan:coverity_scan
+    git checkout coverity_scan
+    git merge --ff --log -m "merge from master to coverity_scan" origin/master
     git push https://$GITAUTH@github.com/plt-tud/r43ples
     git checkout master
 else
