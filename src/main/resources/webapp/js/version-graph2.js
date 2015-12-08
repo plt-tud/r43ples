f8c62d1b934c652ee5a0aebd44dd193447558d67
AngelaWobar <Angela.Wobar@mailbox.tu-dresden.de>
2015-12-08 11:46:44 +0100

    not working version-graph2-version

--- src/main/resources/webapp/js/version-graph2.js
+++ src/main/resources/webapp/js/version-graph2.js
@@ -1,16 +1,130 @@
-function addSpinner(element) {
-	SPINNER_WIDTH = SPINNER_HEIGHT = 60;
-	var spinner_html = jQuery.parseHTML(
-		"<div class='spinner' style='position: absolute; " +
-					"top:"  + (element.position().top  + (element.height()-SPINNER_HEIGHT)/2) + "px;" +
-					"left:" + (element.position().left + (element.width() -SPINNER_WIDTH )/2) + "px;'>" +
-			"<i class='fa fa-spinner fa-pulse fa-3x'></i>" +
-		"</div>");
-	element.after(spinner_html);
-	return element.next();
-}
+function d3_revGraph(
+div_selector) {
+        var commits = {};
+    var revisions = {};
+    var tags = {};
+    var branches = {};
+    var node=[], edge=[];
+    /*Initialization and configuration*/
+    var config = {
+        "opacity_normal": "1.0",
+        "opacity_highlight": "0.6",
+        "colorRange": ["red", "yellow", "green", "blue"],
+        "rotate": -55,
+        "title": "",
+        "title_class": "",
+        "ytitle": "",
+        "xtitle": "",
+        "unit": "",
+        "margin": { "top": 20, "right": 20, "bottom": 65, "left": 40}
+    }
+/*
+    var dConfig = "";
+    try {
+        dConfig = JSON.parse(pConfig);
+    } catch (e) {
+        dConfig = {};
+    }
 
-// Trim revision number if too long for node
+    for (var attrname in dConfig) {
+        config[attrname] = dConfig[attrname];
+    }
+
+    config.title = pTitle;
+    config.ytitle = pYTitle;
+    config.xtitle = pXTitle;*/
+    var colors = d3.scale.category10();
+   
+    var margin = config.margin,
+        width = getWidthwidth(),
+        height = $(div_selector).height() - margin.top - margin.bottom;
+
+    /*Define d3 axes and scales*/
+    var x = d3.scale.ordinal()
+        .rangeRoundPoints([0, width]);
+
+    var y = d3.scale.ordinal()
+        .rangeRoundPoints([height, 0]);
+
+    /*var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(function (v) {
+        return v + config.unit;
+    })
+        .ticks(Math.max(height / 50, 2)); //<-- dynamically adapt number of ticks to height
+    /*create svg-element*/
+    var contsvg = d3.select(div_selector).append("svg")
+        .attr("width", getSVGwidth())
+        .attr("height", getSVGheight())
+        .style("margin", "0px auto")
+        .style("display", "block");
+    var svg = contsvg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    /*append chart-title and x-axis-title*/
+   /* var title = contsvg.append("g");
+    title.append("text")
+        .attr("y", margin.top / 2)
+        .attr("x", getSVGwidth() / 2)
+        .attr("text-anchor", "middle")
+        .attr("class", config.title_class)
+        .text(config.title);
+    /* x- axis- title*/
+   /* title.append("text")
+        .attr("y", getSVGheight() - 15)
+        .attr("x", getSVGwidth() / 2)
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .text(config.xtitle);
+    /*create axes' elements*/
+  /*  svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(" + config.rotate + ")");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "translate(" + (10 - margin.left) + ", " + height / 2 + ")rotate(-90)")
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .text(config.ytitle);*/
+    //div-container for tooltips
+   /* d3.select("#d3_barcharttools_" + pRegionId).append("div")
+        .attr("class", "xstooltip")
+        .attr("id", "tooltip" + pRegionId);*/
+
+    var data = [];
+    var waitresize;
+
+    //getData(refreshData);
+refreshData(json_data);
+    /* FUNCTION DEFINITIONS
+-------------------------------------------------------------------------
+*/
+    function getSVGheight() {
+        return height + margin.top + margin.bottom;
+    }
+
+    function getSVGwidth() {
+        return width + margin.left + margin.right;
+    }
+	
+	function getWidthwidth() {
+		return $(div_selector).width() * .96 - margin.left - margin.right;
+    }
+    // Trim revision number if too long for node
 function trimRevisionNumber(revisionNumber) {
 	if (revisionNumber.length > 5) {
 	    return revisionNumber.substr(0, 2) + ".." + revisionNumber.substr(revisionNumber.length - 2);
@@ -19,136 +133,13 @@ function trimRevisionNumber(revisionNumber) {
 	}
 }
 
-
-// Create a new directed graph
-/** Hauptfunktion, um kompletten Graphen zu erstellen.
- *
- *  _JSON: URL zu JSON-Daten oder Daten selbst
-
- */
-function drawGraph(div_selector, _JSON, _showTags) {
-    // _showTags default is false
-    _showTags = _showTags || false;
-    
-    var g;		//d3 graph
-    var svg, inner;
-	var div_element = $(div_selector);
-    var colors = d3.scale.category10();
-    var commits = {};
-    var revisions = {};
-    var tags = {};
-    var branches = {};
-    var zoom;
-    
-    
-	div_element.html(
-		"<div class='revisionGraphVisualisation'>" +
-		"	<svg><g/></svg>" +
-		"</div>" +
-		"<div class='checkbox'>" +
-        "	<label><input type='checkbox' class='toggle-tags'>Show Tags</label>" +
-	  	"</div>");
-
-	// ChangeListener for tags
-	div_element.find('.toggle-tags').change(function () {
-	    $(this).prop('checked')?showTags():hideTags();
-	});
-
-
-	
-	// http://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
-	function resizeSVG(){
-	    $('.revisionGraphVisualisation svg').each(function() {
-	    	$(this).width($(this).parent().width());
-	    });
-	}
-	$(window).resize( resizeSVG );	 
-	resizeSVG();
-
-	var svg_element = div_element.find('svg');
-	var spinner = addSpinner(svg_element);
-
-    // JSON-Daten mit jQuery laden und parsen
-    $.getJSON(_JSON, function (data, status) {
-        if (status != "success") {
-            alert("Error getting JSON Data: Status " + status);
-        }
-
-        // create  D3 graph
-        g = new dagreD3.graphlib.Graph();
-        g.setGraph({
-          rankdir: "LR",
-        });
-
-
-        // light blue color for tags
-        colors(0);
-		
-		create_revision_model(data);
-
-        // create nodes for every revision
-        Object.keys(revisions).forEach(function (revision) {
-            var value = revisions[revision];
-            value.label = trimRevisionNumber(revisions[revision].revisionNumber);
-            value.height = 25;
-            value.shape = "circle";
-            value.style = "stroke:" + branches[revisions[revision].belongsTo].color + ";";
-            g.setNode(revision, value);
-        });
-
-        // create edge for every commit
-        Object.keys(commits).forEach(function (commit) {
-            for (var i = 0; i < commits[commit].used.length; i++) {
-                var color;
-                // Falls der Commit nur von einer Revision stammt
-                if (commits[commit].used.length == 1) {
-                    // Wird als Farbe fÃ¼r die Kante die Revision genommen, die der Commit erzeugt hat
-                    color = branches[revisions[commits[commit].generated].belongsTo].color;
-                } else {
-                    // Ansonsten die Farbe der Ursprungsrevision
-                    color = branches[revisions[commits[commit].used[i]].belongsTo].color;
-                }
-                g.setEdge(commits[commit].used[i], commits[commit].generated, {
-                    style: "stroke:" + color + ";fill:none;",
-                    arrowheadStyle: "fill:" + color + ";stroke:" + color + ";",
-                    lineInterpolate: "basis"
-                });
-            }
-        });
-
-        createBranches();
-        if (_showTags) {
-            createTags();
-        }
-       
-    	svg = d3.select(div_selector + " svg");
-    	inner = svg.select("g");
-    	
-        // render graph and add to DOM
-        render = new dagreD3.render();
-        render(inner, g);
-        
-        // enable zooming and panning of graph
-        zoom = d3.behavior.zoom().on("zoom", function () {
-        	inner.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-        });
-    	
-        // bind zoom to SVG
-        svg.call(zoom);
-
-        bindQTipToRevisionNodes();
-
-        center();
-        spinner.hide();
-    });
-
-    function create_revision_model(data){
-        // counter for coloring
-    	console.log('JSON ',data);
+function sortByDate (a,b){
+    return a.date-b.date;
+}
+    function buildData(d3json) {
         var j = 1;
-        $.each(data, function (key, value) {
+        $.each(d3json, function (key, value) {
             var types = value["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"];
-            console.log('Typen: ',value["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"], 'key ', key, 'value ', value);
             j++;
             for (var i = 0; i < types.length; i++) {
                 switch (types[i].value) {
@@ -210,123 +201,165 @@ function drawGraph(div_selector, _JSON, _showTags) {
             }
 
         });
-    }
-    
-    var qtip_options = {gravity: 'w', html: true, fade:true, trigger: 'focus', offset: 100};
-
-	 // Center the graph
-	 var center = function () {
-	     // relationf of graph to svg size
-	     var widthscale = svg_element.width() / (g.graph().width + 50);
-	     var heightscale = svg_element.height() / (g.graph().height + 50);
-	     // use smaller scale for adaptation
-	     var scale = widthscale < heightscale ? widthscale : heightscale;
-	     // not more than twice the size
-	     scale = scale > 2 ? 2 : scale;
-	     zoom
-	         .translate([(svg_element.width() - g.graph().width * scale) / 2, (svg_element.height() - g.graph().height * scale) / 2])
-	         .scale(scale)
-	         .event(svg_element);
-	 };
-	
-	
-	 function padStr(i) {
-	     return (i < 10) ? "0" + i : "" + i;
-	 }
-	 
-	 function dateString(date) {
-	 	return padStr(date.getDate()) + "." + padStr((date.getMonth() + 1)) + "." + date.getFullYear() + " " + padStr(date.getHours()) + ":" + padStr(date.getMinutes()) + ":" + padStr(date.getSeconds());
-	 }
-	
-	 // Funktion, die den Inhalt der Revisions-Tooltips erstellt
-	 var revTooltip = function (name, node) {
-	     var tooltip = "<h1>Revision " + node.revisionNumber+"</h1>"+ 
-	         		   "<table class='properties'>";
-	     if (node.commit != null) { 
-		     var date = new Date(commits[node.commit].time);
-		     tooltip += "<tr><th>" + commits[node.commit].title+"</th><td>"+ dateString(date) + "</td><td></td></tr>" +  
-		     "<tr><th>User:</th><td>" + commits[node.commit].wasAssociatedWith + "</td><td><a href='" + commits[node.commit].wasAssociatedWith + "' target='_blank'><i class='fa fa-external-link'></i></a></td></tr>" +
-		     "<tr><th>URL:</th><td>" + node.commit + "</td><td></td></tr>";
-		 }
-	     tooltip+="<tr><th>Add Set:</th><td>" + node.addSet + "</td><td><a href='" + node.addSet + "' target='_blank'><i class='fa fa-external-link'></i></a></td></tr>" +
-	         	  "<tr><th>Delete Set:</th><td>" + node.deleteSet + "</td><td><a href='" + node.deleteSet + "' target='_blank'><i class='fa fa-external-link'></i></a></td></tr></table>";
-	        
-	     
-	     return tooltip;
-	 };
-
-     
-	 
-		
-	 // Funktion, die die Tags als Knoten mit Kanten erstellt
-	 var createTags = function () {
-	     Object.keys(tags).forEach(function (referenceCommit) {
-	         var value = tags[referenceCommit];
-	         value.label = tags[referenceCommit].label;
-	         value.height = 5;
-	         value.style = "stroke:#60b1fc;";
-	         value.labelStyle = "font-size:9px;";
-	         g.setNode(referenceCommit, value);
-             g.setEdge(tags[referenceCommit].head, referenceCommit, {
-                 style: "stroke:#60b1fc;fill:none;stroke-dasharray: 5, 5;",
-                 arrowhead: "undirected",
-                 weight: 2,
-             });
-	     });
-	 };
-	
-	 // Funktion, die die Branches als Knoten mit Kanten erstellt
-	 var createBranches = function () {
-	     Object.keys(branches).forEach(function (branch) {
-	         var value = branches[branch];
-	         value.label = branches[branch].label;
-	         value.height = 5;
-	         value.style = "fill:" + branches[branch].color + ";stroke:#fff;";
-	         value.labelStyle = "font-size:9px;";
-	         g.setNode(branch, value);
-	         g.setEdge(branches[branch].head, branch, {
-	             style: "stroke:" + branches[branch].color + ";fill:none;",
-	             arrowhead: "undirected",
-	             lineInterpolate: "basis"
-	         });
-	     });
-	 };
-	
-    // bind qtip to revision node
-    function bindQTipToRevisionNodes(){
-        inner.selectAll("g.node").filter(function (v) {
-            return revisions[v] != null;
-        })
-            .attr("title", function (v) {
-            	return revTooltip(v, g.node(v))
-            })
-            .each(function() {
-                $(this).qtip({
-                	show: 'click',
-                	hide: 'click'
-                })
-            });
+        console.log("rev", revisions);
+        console.log("commits", commits);
+        console.log("branches", branches);
+        console.log("tags", tags);
         
-     }
+         // create nodes for every revision
+        Object.keys(revisions).forEach(function (revision) {
+            var value = revisions[revision];
+            value.label = trimRevisionNumber(revisions[revision].revisionNumber);
+            value.height = 25;
+            value.shape = "circle";
+            value.style = "stroke:" + branches[revisions[revision].belongsTo].color + ";";
+            //g.setNode(revision, value);
+            node.push(value);
+        });
+
+        // create edge for every commit
+        Object.keys(commits).forEach(function (commit) {
+            for (var i = 0; i < commits[commit].used.length; i++) {
+                var color;
+                // Falls der Commit nur von einer Revision stammt
+                if (commits[commit].used.length == 1) {
+                    // Wird als Farbe fÃ¼r die Kante die Revision genommen, die der Commit erzeugt hat
+                    color = branches[revisions[commits[commit].generated].belongsTo].color;
+                } else {
+                    // Ansonsten die Farbe der Ursprungsrevision
+                    color = branches[revisions[commits[commit].used[i]].belongsTo].color;
+                }
+                /*g.setEdge(commits[commit].used[i], commits[commit].generated, {
+                    style: "stroke:" + color + ";fill:none;",
+                    arrowheadStyle: "fill:" + color + ";stroke:" + color + ";",
+                    lineInterpolate: "basis"
+                });*/
+                var value =commits[commit];
+                value.color=color;
+                value.date= d3.time.format("%Y-%m-%dT%H:%M:%S").parse(commits[commit].time);
+                edge.push(value)
+            }
+        });
+        edge=edge.sort(sortByDate);
+        console.log(node, edge);
+    }
+
+    function Pageload(link) {
+        window.location = link;
+    }
+
+    function getData(f) {
+        /*apex.server.plugin(
+        pAjaxId, {}, {
+            success: f,
+            error: function (d) { /*NOTLÖSUNG: fehlende führende 0 bei Zahlenwerten führt im jQuery-JSONParser zu Fehlern*/
+                //console.log("fail");console.log(d);console.log(d.responseText); 
+          /*      var json = d.responseText.replace(/(\:\.|\:\,)/g, ":0.");
+                f($.parseJSON(json));
+            },
+            dataType: "json"
+        });*/
+        // JSON-Daten mit jQuery laden und parsen
+    $.getJSON(_JSON, function (data, status) {
+        if (status != "success") {
+            alert("Error getting JSON Data: Status " + status);
+        }
+
+        f(data);
+    });
+    }
+
+    //wait some ms before reacting on the resize->decrease compute-weight
+    $(window).on("resize", function () {
+        clearTimeout(waitresize);
+        waitresize = setTimeout(function () {
+            wresize();
+        }, 100);
+    });
+
+    function wresize() {
+        /*height dependend from region-height -> hard coded in style
+	-> get new width
+	-> recompute all affected elements
+	*/
+        width = getWidthwidth();
+        x.rangeRoundPoints([0, width]);
+       /* yAxis.ticks(Math.max(height / 50, 2));
+
+        svg.select("g.x.axis").transition()
+            .call(xAxis)
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(" + config.rotate + ")");
+
+        svg.selectAll("g.y.axis g.tick line").transition()
+            .attr("x2", width);
+        /*svg.select("g.y.axis")
+		.call(yAxis);*/
+
+        /*title.selectAll("text").transition()
+            .attr("x", getSVGwidth() / 2);*/
+
+        d3.select(div_selector + " svg")
+            .attr("width", getSVGwidth())
+            .attr("height", getSVGheight());
+
+        /*var node = svg.selectAll(".tool");
+        node.transition()
+            .attr("transform", function (d) {
+            return "translate(" + x(d.key) + ",0)";
+        })
+            .selectAll(".toolrect")
+            .attr("width", x.rangeBand())
+            .attr("y", function (d) {
+            return y(d.y1);
+        })
+            .attr("height", function (d) {
+            return y(d.y0) - y(d.y1);
+        });
+
+        node.selectAll("g rect").transition()
+        //.attr("y", function (d) {return y(d3.sum(d.states, function (d){return d.value;}));})
+        .attr("width", x.rangeBand())
+        //.attr("height",function (d){return y(0)-y(d3.sum(d.states, function (d){return d.value;})); })
+        ;*/
+
+    }
+
+    /*
+-------------------------------------------------------------------------------------------------------
+*/
+    function refreshData(d3json) {
+        /*No data found - section*/
+        if (!d3json || d3json.length == 0) {
+            contsvg.append("text").attr("class", "NoData")
+                .attr("x", getSVGwidth() / 2)
+                .attr("y", getSVGheight() / 2)
+                .attr("dy", ".35em")
+                .attr("text-anchor", "middle")
+                .text("No Data");
+            return;
+        }
+        contsvg.select("text.NoData").remove();
+
+        buildData(d3json); //<-- get useable data-array out of json-object
+        
+
+        var edges = svg.selectAll(".edge")
+            .data(edge);
+			
+
+        /*INSERT-section ->visualize new label*/
+        var edgesEnter = egde.enter().append("g")
+            .attr("class", "edge")
+            .attr("transform", function (d) {
+            return "translate(" + x(d.date) + ",0)";
+        });
+
+    /*UPDATE-section ->update existing labels*/
+    /*DELETE-section -> remove no longer existing labels*/
     
-                
-	
-	 // Funktion um Tags einzublenden
-	 var showTags = function () {
-	     createTags();
-	     render(inner, g);
-	 };
-	
-	 // Funktion um Tags auszublenden
-	 var hideTags = function () {
-	     // remove tag nodes from d3
-		 inner.selectAll("g.node").filter(function (v) {
-	         return tags[v] != null;
-	     })
-	         .each(function (v) {
-	             g.removeNode(v);
-	         });
-	     render(inner, g);
-	 };    
-    
-};
+}
+}
