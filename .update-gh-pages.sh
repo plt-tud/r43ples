cdefc43985536aa6cb8ef62735f6e7d8d047496e
Markus Graube <markus.graube@tu-dresden.de>
2017-07-24 13:57:15 +0200

    Try to fix token

--- .update-gh-pages.sh
+++ .update-gh-pages.sh
@@ -18,7 +18,7 @@ git config --global user.email "r43ples-travis-ci@users.noreply.github.com"
 git config --global user.name "r43ples travis-ci"
 git config --global push.default simple 
 
-git clone -b gh-pages https://$GITAUTH@github.com/plt-tud/r43ples gh-pages
+git clone -b gh-pages https://github.com/plt-tud/r43ples gh-pages
 rm -rf gh-pages/site
 cp -r target/site gh-pages/site
 
@@ -28,6 +28,6 @@ cat README.md >> gh-pages/index.md
 cd gh-pages
 git add -A *
 git commit -am "javadoc updated by travis-ci"
-git push https://$GITAUTH@github.com/plt-tud/r43ples
+git push https://$GITHUB_AUTH_TOKEN@github.com/plt-tud/r43ples
 cd ..
 rm -rf r43ples
