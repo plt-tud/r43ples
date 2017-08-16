23c3a6341baa89484782a24005c7b6c91e550ac9
stephanhensel <stephhens@gmx.de>
2017-08-16 14:59:22 +0200

    JavaDoc added

--- src/main/java/de/tud/plt/r43ples/management/R43plesCommit.java
+++ src/main/java/de/tud/plt/r43ples/management/R43plesCommit.java
@@ -3,26 +3,46 @@ package de.tud.plt.r43ples.management;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Provides information of a commit.
+ *
+ * @author Markus Graube
+ */
 public class R43plesCommit extends R43plesRequest{
-	
+
+	/** The pattern modifier. **/
 	private final int patternModifier = Pattern.DOTALL + Pattern.MULTILINE + Pattern.CASE_INSENSITIVE;
+	/** The pattern to extract the user name. **/
 	private final Pattern patternUser = Pattern.compile(
 			"USER\\s*\"(?<user>[^\"]*)\"",
 			patternModifier);
+	/** The pattern to extract the commit message. **/
 	private final Pattern patternCommitMessage = Pattern.compile(
 			"MESSAGE\\s*\"(?<message>[^\"]*)\"", 
 			patternModifier);
-
+	/** The associated user name of the commit. **/
 	public String user;
+	/** The message of the commit. **/
 	public String message;
-	
+
+
+	/**
+	 * The constructor.
+	 *
+	 * @param request the request received by R43ples
+	 */
 	public R43plesCommit(final R43plesRequest request){
 		super(request.query_r43ples, request.format);
 		this.query_sparql = request.query_sparql;
 		this.extractUser();
 		this.extractMessage();
 	}
-	
+
+	/**
+	 * Extracts the user name out of the given query.
+	 *
+	 * @return the extracted user name
+	 */
 	private String extractUser() {
 		Matcher userMatcher = patternUser.matcher(this.query_sparql);
 		if (userMatcher.find()) {
@@ -32,6 +52,11 @@ public class R43plesCommit extends R43plesRequest{
 		return this.user;
 	}
 
+	/**
+	 * Extracts the message out of the given query.
+	 *
+	 * @return the extracted message
+	 */
 	private String extractMessage() {
 		Matcher messageMatcher = patternCommitMessage.matcher(this.query_sparql);
 		if (messageMatcher.find()) {
--- src/main/java/de/tud/plt/r43ples/management/R43plesRequest.java
+++ src/main/java/de/tud/plt/r43ples/management/R43plesRequest.java
@@ -3,62 +3,112 @@ package de.tud.plt.r43ples.management;
 import java.util.regex.Pattern;
 
 
+/**
+ * Provides information of a request received by R43ples.
+ *
+ * @author Markus Graube
+ */
 public class R43plesRequest {
-	
-private final static int patternModifier = Pattern.DOTALL + Pattern.MULTILINE + Pattern.CASE_INSENSITIVE;
-	
+
+	/** The pattern modifier. **/
+	private final static int patternModifier = Pattern.DOTALL + Pattern.MULTILINE + Pattern.CASE_INSENSITIVE;
+	/** Pattern for SELECT, ASK and CONSTRUCT queries. **/
 	private final Pattern patternSelectAskConstructQuery = Pattern.compile(
 			"(?<type>SELECT|ASK|CONSTRUCT).*WHERE\\s*\\{(?<where>.*)\\}", 
 			patternModifier);
+    /** Pattern for UPDATE queries. **/
 	private final Pattern patternUpdateQuery = Pattern.compile(
 			"(?<action>INSERT|DELETE).*<(?<graph>[^>]*)>",
 			patternModifier);
+    /** Pattern for CREATE GRAPH queries. **/
 	private final Pattern patternCreateGraph = Pattern.compile(
 			"CREATE\\s*(?<silent>SILENT)?\\s*GRAPH\\s*<(?<graph>[^>]*)>",
 			patternModifier);
+    /** Pattern for DROP GRAPH queries. **/
 	private final Pattern patternDropGraph = Pattern.compile(
 			"DROP\\s*(?<silent>SILENT)?\\s*GRAPH\\s*<(?<graph>[^>]*)>",
 			patternModifier);
+    /** Pattern for BRANCH or TAG queries. **/
 	private final Pattern patternBranchOrTagQuery = Pattern.compile(
 			"(?<action>TAG|BRANCH)\\s*GRAPH\\s*<(?<graph>[^>\\?]*)(\\?|>)(\\s*REVISION\\s*\"|revision=)(?<revision>([^\">]+))(>|\")\\s*TO\\s*\"(?<name>[^\"]*)\"",
 			patternModifier);
-
+    /** Pattern for MERGE queries. **/
 	private final Pattern patternMergeQuery =  Pattern.compile(
 			"(MERGE|MERGE FF|REBASE)\\s*(AUTO|MANUAL)?\\s*GRAPH\\s*<([^>]*?)>\\s*(SDD\\s*<([^>]*?)>)?\\s*BRANCH\\s*\"([^\"]*?)\"\\s*INTO\\s*\"([^\"]*?)\"",
 			patternModifier);
 	
-	
+	/** The original query received by R43ples. **/
 	public final String query_r43ples;
+	/** The format of the original query. **/
 	public final String format;
+	/** The resulting SPARQL query. **/
 	public String query_sparql;
-	
-	public R43plesRequest(final String query, final String format) {
+
+
+    /**
+     * The constructor.
+     *
+     * @param query the query received by R43ples
+     * @param format the query format
+     */
+    public R43plesRequest(final String query, final String format) {
 		this.query_r43ples = query;
 		this.format = format;
 		this.query_sparql = query;
 	}
-	
-	public boolean isSelectAskConstructQuery(){
-	  return patternSelectAskConstructQuery.matcher(query_sparql).find();
+
+    /**
+     * Test if the SPARQL query is a SELECT, ASK or CONSTRUCT query.
+     *
+     * @return true if the query is a SELECT, ASK or CONSTRUCT query.
+     */
+    public boolean isSelectAskConstructQuery(){
+        return patternSelectAskConstructQuery.matcher(query_sparql).find();
 	}
-	
+
+    /**
+     * Test if the SPARQL query is an UPDATE query.
+     *
+     * @return true if the query is an UPDATE query.
+     */
 	public boolean isUpdateQuery() {
-		return patternUpdateQuery.matcher(query_sparql).find();
+        return patternUpdateQuery.matcher(query_sparql).find();
 	}
-	
+
+    /**
+     * Test if the SPARQL query is a CREATE GRAPH query.
+     *
+     * @return true if the query is a CREATE GRAPH query.
+     */
 	public boolean isCreateGraphQuery() {
 		return patternCreateGraph.matcher(query_sparql).find();
 	}
-	
+
+    /**
+     * Test if the SPARQL query is a DROP GRAPH query.
+     *
+     * @return true if the query is a DROP GRAPH query.
+     */
 	public boolean isDropGraphQuery() {
-		return patternDropGraph.matcher(query_sparql).find();	
+		return patternDropGraph.matcher(query_sparql).find();
 	}
-	
+
+    /**
+     * Test if the SPARQL query is a BRANCH or TAG query.
+     *
+     * @return true if the query is a BRANCH or TAG query.
+     */
 	public boolean isBranchOrTagQuery() {
 		return patternBranchOrTagQuery.matcher(query_sparql).find();
 	}
-	
+
+    /**
+     * Test if the SPARQL query is a MERGE query.
+     *
+     * @return true if the query is a MERGE query.
+     */
 	public boolean isMergeQuery() {
 		return patternMergeQuery.matcher(query_sparql).find();
 	}
+
 }
